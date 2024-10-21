3df6a6350e07de808d86eb1d09bcbaccc1bfdccc
nickanhhuy <nguyenvh@sheridancollege.ca>
2024-10-21 16:18:36 -0400

    Unversioned Files

---
+++ cmake-build-debug/CMakeFiles/assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1.dir/function.c.o"
+  "CMakeFiles/assignment1.dir/function.c.o.d"
+  "CMakeFiles/assignment1.dir/main.c.o"
+  "CMakeFiles/assignment1.dir/main.c.o.d"
+  "assignment1.exe"
+  "assignment1.pdb"
+  "libassignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
